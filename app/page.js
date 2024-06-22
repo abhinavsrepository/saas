4e950bd6695940ffe326bb5d62f09e920271edaf
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-06-22 18:47:26 +0530

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
       <h2>i will be best in 6 months</h2>
+      <Button> i wiill be great in next 6 month
+      </Button>
     </div>
   );
 }
